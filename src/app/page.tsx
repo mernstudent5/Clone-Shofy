1c6b2605ad00f41c6e6de28803a1f898656d0df1
mernstudent5 <mernstudent5@gmail.com>
2024-08-16 09:48:42 +0600

    Shofy Clone

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import BottomHeader from "@/components/header/BottomHeader";
+import Banner from "@/components/pages/home/Banner";
+
+export default function Home() {
+  return (
+    <main>
+      <BottomHeader />
+      <Banner />
+    </main>
+  );
+}
